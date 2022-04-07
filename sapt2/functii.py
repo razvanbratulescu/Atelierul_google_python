21e4c962859b782180260d072223304a49ed4e14
razvan <razvan_bratulescu@yahoo.com>
2022-04-07 19:08:28 +0300

    branch

---
+++ sapt2/functii.py
@@ -0,0 +1,16 @@
+# print("mesaj nr {}".format(1))
+# raspuns_utilizator = input("care este numele tau: ")
+# print(raspuns_utilizator)
+#
+# def functia_mea(param_1):
+#     pass
+
+def suma(a,b):
+    if type(a)== str:
+        return "ana"
+    return a + b
+#cand ajunge la return se iese din functie
+
+suma_mea = suma("1" , 2)
+print(suma_mea)
+
