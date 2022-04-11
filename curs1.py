78ba4b27e3bc6fc5dd775b14a23198cb661598ef
razvan <razvan_bratulescu@yahoo.com>
2022-04-11 10:27:41 +0300

    CNP

--- curs1.py
+++ curs1.py
@@ -1,1 +1,1 @@
-print("cursu")
+print("curs")
--- sapt2/functii.py
+++ sapt2/functii.py
@@ -5,12 +5,55 @@
 # def functia_mea(param_1):
 #     pass
 
-def suma(a,b):
-    if type(a)== str:
-        return "ana"
-    return a + b
-#cand ajunge la return se iese din functie
+# def suma(a,b) -> (int, int):
+#     """
+#     :param a: primul numar
+#     :param b: al doile numar
+#     :return: suma, diferenta
+#     """
+#     return a + b, a - b
+# #cand ajunge la return se iese din functie
+#
+# suma_mea, diferenta = suma(1 , 2)
+# print(suma_mea, diferenta)
 
-suma_mea = suma("1" , 2)
-print(suma_mea)
+# def my_function(*param_1):
+#     print(type(param_1))
+#     return param_1
+# print(my_function("string", "strin1", "string2"))
 
+# def suma_nr_infinite(param1, param2, *var):
+#     sum = 0
+#     for item in var:
+#         suma += item
+#     return suma
+#
+# print(suma_nr_infinite(1 ,2,3,4,5,6,7))
+#
+# def catalog(*args, **kwargs):#tuplu si dictionar
+#     print(type(kwargs))
+#     return args, kwargs
+#
+# print(catalog(1,2, nume="ION", prenume="Vasile", varsta="12"))
+a, b = 10, 20
+min = a if a<b else b
+print(min)
+
+lista_produse = ["ciocolata", "mere", "apa", "cuvant"]
+# lista_noua = []
+# for x in lista_produse:
+#     if "a" in x:
+#         lista_noua.append(x)
+
+#lista_noua = [x for x in lista_produse if "a" in x]
+# lista_noua = [x if "a" in x else "b" for x in lista_produse]
+#
+#
+# print(lista_noua)
+
+# min = None
+# if min:= a < b:
+#     print(min)
+#     min = a
+# else:
+#     min = b
